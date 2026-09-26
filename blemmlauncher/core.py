0f4bbea4c0c17784a22e8372c2248335291d1168
Website development <wowgrayhaha@outlook.com>
2026-09-25 23:40:24 -0700

    Show managed server in Minecraft multiplayer on launch

--- blemmlauncher/core.py
+++ blemmlauncher/core.py
@@ -1353,7 +1353,7 @@ def launch(version_id, username="Blemm", ram="2G", optifine=False):
     even if the game crashes.
     """
 
-    m = manifest()
+    # Make the managed server appear in Minecraft Multiplayer for every launch.\n    try:\n        from . import instances as _instances\n        _instances.ensure_online_server()\n    except Exception as e:\n        log("Could not update the Minecraft Multiplayer server list: " + str(e))\n\n    m = manifest()
 
     vid = resolve_version(version_id, m)
 
